e29c67ec544d112ab607f1a423693cf3fce28f9b
kikuchito <80461493+kikuchito@users.noreply.github.com>
2024-04-16 17:23:51 +0300

    Update dsp_mult.sv
    доделал умножитель

--- dsp_mult.sv
+++ dsp_mult.sv
@@ -7,7 +7,7 @@
     input  logic [35:0] w_i,
     input  logic        data_valid_i,
     output logic        data_valid_o,
-    output logic [87:0] butterfly_stage_o
+    output logic [49:0] butterfly_stage_o
   );
   
   logic [24:0] s_input_ff_re;
@@ -20,19 +20,26 @@
   logic [24:0] s_re_reg2;
   logic [17:0] w_re_reg1;
   logic [17:0] w_re_reg2;
-  logic [43:0] mult_re;
-  logic [43:0] mult_reg_re;
-  logic [43:0] sum_re;
-  logic [43:0] sum_reg_re;
+  logic [47:0] mult_re;
+  logic [47:0] mult_reg_re;
+  logic [47:0] sum_re;
+  logic [47:0] sum_reg_re;
   logic [24:0] s_im_reg1;
-  logic [24:0] s_im_reg2;
-  logic [43:0] mult_im;
-  logic [43:0] mult_reg_im1;
-  logic [43:0] mult_reg_im2;
+  logic [24:0] w_im_reg1;
+  logic [47:0] mult_im;
+  logic [47:0] mult_reg_im1;
+  logic [47:0] mult_reg_im2;
 
   //regs for image part
-  
-  
+  logic [24:0] w_im_reg2;
+  logic [47:0] mult_Ab;
+  logic [47:0] mult_aB;
+  logic [47:0] reg1_mult_aB;
+  logic [47:0] reg2_mult_aB;
+  logic [47:0] reg_mult_Ab;
+  logic [47:0] sum_im;
+  logic [47:0] sum_reg_im;
+
   logic input_valid_ff;
   logic data_valid_stage_1_ff;
   logic data_valid_stage_2_ff;
@@ -45,6 +52,7 @@
   assign w_input_ff_re = w_i[35:18];
   assign s_input_ff_im = stage_i[24:0];
   assign w_input_ff_im = w_i[17:0];
+  
 
   always_ff @ (posedge clk_i or posedge rst_i)
     if (rst_i)
@@ -76,33 +84,35 @@
     w_re_reg1 <= w_input_ff_re;
     s_im_reg1 <= s_input_ff_im;
     w_im_reg1 <= w_input_ff_im;
-
   end
     
-  assign mult_im = s_im_reg1 * w_im_reg1;
-
-  always_ff @(posedge clk3) 
-    mult_reg_im1 <= mult_im;
-
-  always_ff @(posedge clk3) 
-    mult_reg_im2 <= mult_reg_im1;
+  assign mult_im = $signed(s_im_reg1) * $signed(w_im_reg1);
 
   always_ff @(posedge clk2) begin
+    mult_reg_im1 <= mult_im;
     s_re_reg2 <= s_re_reg1;
     w_re_reg2 <= w_re_reg1;
+    w_im_reg2 <= w_im_reg1;
+    reg1_mult_aB <= mult_aB;
+  end
+  
+  always_ff @(posedge clk3)
+    mult_reg_im2 <= mult_reg_im1;
+    mult_reg_re <= mult_re;
+    reg2_mult_aB <= reg1_mult_aB;
+
+  assign mult_Ab = $signed(w_im_reg2) * $signed(s_re_reg2);
+  assign mult_aB = $signed(s_im_reg1) * $signed(w_im_reg1);
+  assign mult_re = $signed(s_re_reg2) * $signed(w_re_reg2);
+  assign sum_re = $signed(mult_reg_re) + $signed(mult_reg_im2);
+  assign sum_im = $signed(reg2_mult_aB) + $signed(reg_mult_Ab);
+
+  always_ff @(posedge clk4) begin
+    sum_reg_re <= sum_re;
+    sum_reg_im <= sum_im;
   end
 
-  assign mult_re = s_re_reg2 * w_re_reg2;
-
-  always_ff @(posedge clk3)
-    mult_reg_re <= mult_re;
-
-  assign sum_re = mult_reg_re + mult_reg_im2;
-
-  always_ff @(posedge clk4)
-    sum_reg_re = sum_re
-
-  assign butterfly_stage_o = {sum_reg_re, sum_reg_im};
+  assign butterfly_stage_o = {sum_reg_re[47:23], sum_reg_im[47:23]};
   assign clk1 = data_valid_i && clk_i;
   assign clk2 = data_valid_stage_1_ff && clk_i;
   assign clk3 = data_valid_stage_2_ff && clk_i;
