7aa626173f2d6f1ecb27ed475f3ff9bccbeb5963
kikuchito <80461493+kikuchito@users.noreply.github.com>
2024-05-06 14:46:03 +0300

    Update tb_butterfly_stage.sv

--- tb_butterfly_stage.sv
+++ tb_butterfly_stage.sv
@@ -2,12 +2,19 @@
 
 module tb_butterfly();
 
-logic  [49:0] signal_tb      [7:0];
-logic  [49:0] final_stage_tb [7:0];
-logic         clk;
-logic         rst;
+logic [49:0] signal_store_tb      [7:0];
+logic [49:0] final_stage_store_tb [7:0];
+logic [49:0] signal_tb;
+logic [ 2:0] num_of_signal_tb;
+logic [49:0] final_stage_tb;
+logic [2:0]  final_num_tb;
+logic        clk;
+logic        rst;
 
 parameter CLK_PERIOD = 20; 
+
+parameter FILE_PATH_IN = "E:/vivado/vivado_proj/FFT/mem_in.txt";
+parameter FILE_PATH_OUT = "E:/vivado/vivado_proj/FFT/mem_out.txt";
 // always begin
 //     #10 clk_i = ~clk_i;
 // end
@@ -17,33 +24,64 @@ initial begin
   forever #(CLK_PERIOD / 2) clk = ~clk;
 end
 
-butterfly_stage butterfly_inst(
+butterfly_stage butterfly_stage_dut(
 .clk_i( clk ),
 .rst_i( rst ),
 .signal( signal_tb ),
-.final_stage( final_stage_tb )
+.final_stage( final_stage_tb ),
+.num_of_signal(num_of_signal_tb),
+.final_num(final_num_tb)
 );
 
-initial 
-begin
-rst = 1;
+initial begin
+  rst = 1;
 
-#20
-rst = 0;
+  #20
+  rst = 0;
 
-#20
-signal_tb[0] = 50'b00000000000000000000000010000000000000000000000001;
-signal_tb[1] = 50'b00000000000000000000000010000000000000000000000000;
-signal_tb[2] = 50'b00000000000000000000000000000000000000000000000001;
-signal_tb[3] = 50'b00000000000000000000000000000000000000000000000000;
-signal_tb[4] = 50'b00000000000000000000000110000000000000000000000000;
-signal_tb[5] = 50'b10000000000000000000000110000000000000000000000001;
-signal_tb[6] = 50'b00000000000000000000000010000000000000000000000000;
-signal_tb[7] = 50'b00000000000000000000000101000000000000000000000010;
+  $readmemb(FILE_PATH_IN, signal_store_tb, 0);
 
+  for (int i=0; i < 8; ++i) begin
+    #20
+    num_of_signal_tb = i;
+    signal_tb = signal_store_tb[i];
+  end
 
+  if (final_num_tb >= '0) begin
+  final_stage_store_tb[final_num_tb] = final_stage_tb;
+
+  // #20
+  // $writememb(FILE_PATH_OUT, , 0);
 
 end
+
+
+// #20
+// num_of_signal_tb = 3'd0;
+// signal_tb = signal_store_tb[0];
+// #20
+// num_of_signal_tb = 3'd1;
+// signal_tb = ;
+// #20
+// num_of_signal_tb = 3'd2;
+// signal_tb = ;
+// #20
+// num_of_signal_tb = 3'd3;
+// signal_tb = ;
+// #20
+// num_of_signal_tb = 3'd4;
+// signal_tb = ;
+// #20
+// num_of_signal_tb = 3'd5;
+// signal_tb = ;
+// #20
+// num_of_signal_tb = 3'd6;
+// signal_tb = ;
+// #20
+// num_of_signal_tb = 3'd7;
+// signal_tb = ;
+
+
 
 
 
