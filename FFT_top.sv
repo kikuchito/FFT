2d8a287eb506eb0b378b404959add2b64481d964
kikuchito <80461493+kikuchito@users.noreply.github.com>
2024-05-06 15:09:35 +0300

    Add files via upload

---
+++ FFT_top.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06.05.2024 15:08:25
+// Design Name: 
+// Module Name: FFT_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FFT_top(
+
+    );
+endmodule
