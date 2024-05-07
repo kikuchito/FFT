c6ca553bbb364751815c4de9c7633e1aacf42104
kikuchito <80461493+kikuchito@users.noreply.github.com>
2024-05-07 17:30:45 +0300

    Update butterfly_stage.sv

--- butterfly_stage.sv
+++ butterfly_stage.sv
@@ -1,11 +1,13 @@
 `timescale 1ns / 1ps
 
 
-  module butterfly_stage(
+  module butterfly_stage #(parameter MAX_NUM_OF_SIGNALS = 3'd7)(
     input  logic         clk_i,
     input  logic         rst_i,
+    input  logic         tvalid,
     input  logic  [49:0] signal,
     input  logic  [ 2:0] num_of_signal,
+    input  logic         signal_flag,
     output signed [49:0] final_stage, 
     output logic  [ 2:0] final_num
   );
@@ -25,66 +27,19 @@
   logic signed [49:0] stage3_reg      [7:0];
 
   logic  [ 2:0] final_num;
+  // logic         end_flag;
+  // logic         end_flag_reg;
 
-  //regs for dsp_mult
   logic       valid_reg_i [11:0];
   logic       valid_reg_o [11:0];
-  // logic [2:0] num_of_signal;
-   // logic [2:0] stage_counter_i;
-  // logic [2:0] stage_counter_o;
-  // logic [2:0] w_counter;
-  // logic [2:0] num_i;
-  // logic [2:0] num_o;
-  // logic [2:0] w_num;
 
-  // always_ff @(posedge clk_i) begin
-  //   if (rst_i || stage_counter_i == 3'd7)
-  //     stage_counter_i <= 3'd4;
-    
-  //   else if (valid_reg_o)
-  //     stage_counter_i <= stage_counter_i + '1;
-  // end
-
-  // assign num_i = stage_counter_i;
-
-  // always_ff @(posedge clk_i) begin
-  //   if (rst_i || stage_counter_o == 3'd7)
-  //     stage_counter_o <= 3'd4;
-    
-  //   else if (valid_reg_o)
-  //     stage_counter_o <= stage_counter_o + '1;
-  // end
-
-  // assign num_o = stage_counter_i;
-
-  // always_ff @(posedge clk_i) begin
-  //   if (rst_i || w_counter == 3'd3)
-  //     w_counter <= 3'd0;
-    
-  //   else if (valid_reg_o)
-  //     w_counter <= w_counter + '1;
-  // end
-
-  // assign w_num = w_counter;
 
   always_ff @(posedge clk_i) begin
-    if (rst_i) begin
-      signal_reg[0] <= '0;
-      signal_reg[1] <= '0; 
-      signal_reg[2] <= '0; 
-      signal_reg[3] <= '0;
-      signal_reg[4] <= '0; 
-      signal_reg[5] <= '0; 
-      signal_reg[6] <= '0;
-      signal_reg[8] <= '0;    
-    end
-
-    else begin
+    if (signal_flag == 0) 
       signal_reg[num_of_signal][49:0] <= signal;
-    end
+    else if (signal_flag) 
+      signal_reg[MAX_NUM_OF_SIGNALS][49:0] <= signal;
   end
-
-
 
   always_comb begin
     w[0] = { 18'b01_0000000000000000, 18'b0 };
@@ -276,18 +231,19 @@
   );
 
   always_ff @(posedge clk_i) begin
-    if (rst_i)
+    if (rst_i || !signal_flag)
       final_num <= '0;
-    else if (final_stage_reg[7] != 49'bx)
+    else if (final_stage_reg[7] != 49'bx && final_num != MAX_NUM_OF_SIGNALS)
       final_num <= final_num + 1'd1;
   end
 
+  // always_ff @(posedge clk_i) begin
+  //   if (rst_i)
+  //     end_flag_reg <= '0;
+  //   else if (final_stage_reg[7] != 49'bx)
+  //     end_flag_reg <= '1;
+  // end
+
   assign final_stage = final_stage_reg[final_num];
   
-  // assign stage1[4] = { stage_reg[4][49:25] * $signed(w[0][36:18]) - stage_reg[4][24:0] * $signed(w[0][36:18]), stage_reg[4][49:25] * w[0][36:18] + stage_reg[4][24:0] * w[0][36:18] };
-  // assign stage1[5] = { stage_reg[5][49:25] * $signed(w[1][36:18]) - stage_reg[5][24:0] * $signed(w[1][17:0]), stage_reg[5][49:25] * w[1][36:18] + stage_reg[5][24:0] * w[1][17:0] };
-  // assign stage1[6] = { stage_reg[6][49:25] * $signed(w[2][17:0]) - stage_reg[6][24:0] * $signed(w[2][17:0]), stage_reg[6][49:25] * w[2][17:0] + stage_reg[6][24:0] * w[2][17:0] };
-  // assign stage1[7] = { stage_reg[7][49:25] * $signed(w[3][36:18]) - stage_reg[7][24:0] * $signed(w[3][17:0]), stage_reg[7][49:25] * w[3][36:18] + stage_reg[7][24:0] * w[3][17:0] };
-
-
   endmodule
