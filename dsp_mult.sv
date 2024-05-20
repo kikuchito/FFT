a807f3c285e0b4fd06afb68389e76b631368a546
kikuchito <80461493+kikuchito@users.noreply.github.com>
2024-05-20 15:25:19 +0300

    Update dsp_mult.sv

--- dsp_mult.sv
+++ dsp_mult.sv
@@ -1,44 +1,44 @@
 `timescale 1ns / 1ps
 
   module dsp_mult(
-    input  logic        clk_i,
-    input  logic        rst_i,
-    input  logic [49:0] stage_i,
-    input  logic [35:0] w_i,
-    input  logic        data_valid_i,
-    output logic        data_valid_o,
-    output logic [49:0] butterfly_stage_o
+    input  logic         clk_i,
+    input  logic         rst_i,
+    input  logic  [49:0] stage_i,
+    input  logic  [35:0] w_i,
+    input  logic         data_valid_i,
+    output logic         data_valid_o,
+    output signed [49:0] butterfly_stage_o
   );
   
-  logic [24:0] s_input_ff_re;
-  logic [17:0] w_input_ff_re;
-  logic [24:0] s_input_ff_im;
-  logic [17:0] w_input_ff_im;
+  logic        [24:0] s_input_ff_re;
+  logic        [17:0] w_input_ff_re;
+  logic        [24:0] s_input_ff_im;
+  logic        [17:0] w_input_ff_im;
   
   //regs for real part
-  logic [24:0] s_re_reg1;
-  logic [24:0] s_re_reg2;
-  logic [17:0] w_re_reg1;
-  logic [17:0] w_re_reg2;
-  logic [47:0] mult_re;
-  logic [47:0] mult_reg_re;
-  logic [47:0] sum_re;
-  logic [47:0] sum_reg_re;
-  logic [24:0] s_im_reg1;
-  logic [24:0] w_im_reg1;
-  logic [47:0] mult_im;
-  logic [47:0] mult_reg_im1;
-  logic [47:0] mult_reg_im2;
+  logic        [24:0] s_re_reg1;
+  logic        [24:0] s_re_reg2;
+  logic        [17:0] w_re_reg1;
+  logic        [17:0] w_re_reg2;
+  logic        [42:0] mult_re;
+  logic        [42:0] mult_reg_re;
+  logic signed [47:0] sum_re;
+  logic signed [47:0] sum_reg_re;
+  logic        [24:0] s_im_reg1;
+  logic        [17:0] w_im_reg1;
+  logic        [42:0] mult_im;
+  logic        [42:0] mult_reg_im1;
+  logic        [42:0] mult_reg_im2;
 
   //regs for image part
-  logic [24:0] w_im_reg2;
-  logic [47:0] mult_Ab;
-  logic [47:0] mult_aB;
-  logic [47:0] reg1_mult_aB;
-  logic [47:0] reg2_mult_aB;
-  logic [47:0] reg_mult_Ab;
-  logic [47:0] sum_im;
-  logic [47:0] sum_reg_im;
+  logic        [17:0] w_im_reg2;
+  logic        [42:0] mult_Ab;
+  logic        [42:0] mult_aB;
+  logic        [42:0] reg1_mult_aB;
+  logic        [42:0] reg2_mult_aB;
+  logic        [42:0] reg_mult_Ab;
+  logic signed [47:0] sum_im;
+  logic signed [47:0] sum_reg_im;
 
   logic input_valid_ff;
   logic data_valid_stage_1_ff;
@@ -86,7 +86,7 @@
     w_im_reg1 <= w_input_ff_im;
   end
     
-  assign mult_im = $signed(s_im_reg1) * $signed(w_im_reg1);
+ 
 
   always_ff @(posedge clk2) begin
     mult_reg_im1 <= mult_im;
@@ -102,19 +102,21 @@
     reg2_mult_aB <= reg1_mult_aB;
     reg_mult_Ab <= mult_Ab;
   end
-
+  
+  assign mult_im = $signed(s_im_reg1) * $signed(w_im_reg1);
   assign mult_Ab = $signed(w_im_reg2) * $signed(s_re_reg2);
-  assign mult_aB = $signed(s_im_reg1) * $signed(w_im_reg1);
+  assign mult_aB = $signed(s_im_reg1) * $signed(w_re_reg1);
   assign mult_re = $signed(s_re_reg2) * $signed(w_re_reg2);
-  assign sum_re = $signed(mult_reg_re) + $signed(mult_reg_im2);
-  assign sum_im = $signed(reg2_mult_aB) + $signed(reg_mult_Ab);
+  assign sum_re = $signed(mult_reg_re) - $signed(mult_reg_im2);
+  assign sum_im = $signed(reg_mult_Ab) + $signed(reg2_mult_aB);
 
   always_ff @(posedge clk4) begin
     sum_reg_re <= sum_re;
     sum_reg_im <= sum_im;
   end
 
-  assign butterfly_stage_o = {sum_reg_re[47:23], sum_reg_im[47:23]};
+  // assign butterfly_stage_o = {sum_reg_re[47:23], sum_reg_im[47:23]};
+  assign butterfly_stage_o = {sum_reg_re[40:16], sum_reg_im[40:16]};
   assign clk1 = data_valid_i && clk_i;
   assign clk2 = data_valid_stage_1_ff && clk_i;
   assign clk3 = data_valid_stage_2_ff && clk_i;
